1b053dec4aa47fd5fd299cfdf137790f585d8d70
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-11-06 17:39:29 +0200

    hot fix

--- components/checkout/PickTime.vue
+++ components/checkout/PickTime.vue
@@ -59,12 +59,13 @@ export default {
             this.currentMinute = "";
             if (this.isSameDay) {
                 const isPm = newHour.includes('pm')
+                console.log(new Date().getHours(), (parseInt(newHour) + 12));
                 if (isPm) {
-                    if (new Date().getHours() == (parseInt(newHour) + 12)) {
+                    if (new Date().getHours() + 1 == (parseInt(newHour) + 12)) {
                         this.minutes = timeChecker('asap', new Date).minutes
                     }
                 } else {
-                    if (new Date().getHours() == (parseInt(newHour))) {
+                    if (new Date().getHours() + 1 == (parseInt(newHour))) {
                         this.minutes = timeChecker('asap', new Date).minutes
                     }
                 }
