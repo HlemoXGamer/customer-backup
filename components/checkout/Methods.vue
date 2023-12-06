96c316fb88da2dd39b977525313254d68b12103b
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-12-06 15:47:49 +0200

    add credit card method

--- components/checkout/Methods.vue
+++ components/checkout/Methods.vue
@@ -38,6 +38,10 @@ export default {
           text: "Bookeey",
         },
         {
+          icon: "credit_card.png",
+          text: "Credit",
+        },
+        {
           icon: "apple_pay.png",
           text: "Apple Pay"
         },
